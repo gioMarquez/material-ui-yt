da9bfab8389c66f59ab3f55380689b473900d894
Jhovanny <jhovanny.fragoso@codiceglobal.com>
2024-03-19 17:45:51 -0600

    Feat: Agregando Tipografia de MUI y fuentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 // Maneras de importar de Material UI
 // import Button from '@mui/material/button'
-import { Button, Container } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
 
 const App = () => {
   return (
     <Container sx={{ border: 3, boxShadow: 3} }>
-      <div className="title">
+      <div className="">
         <h1>App</h1>
+        <Typography variant="h1" > App h1 con componente </Typography>
+        <Typography variant="h1" component="span" mt={3} textAlign={"center"}> App h1 con componente </Typography>
+        <Typography variant="h1" component="h2" textAlign={"right"}> App h1 con componente </Typography>
+
 
         <Button variant="contained"> Mi primer botón </Button>
       </div>
